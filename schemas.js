22aa6a2109c42985ca41b2ddd1b775676384fccd
raymanhuang_ <imarayman123@gmail.com>
2023-07-27 04:04:31 -0400

    fixed bugs

--- schemas.js
+++ schemas.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const { number } = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -10,3 +11,9 @@ module.exports.campgroundSchema = Joi.object({
     }).required()
 });
 
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
